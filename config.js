93de3fc24c68fc0792e255b91d164ade582b66db
igyomee <ig.yomee@gmail.com>
2026-05-27 11:30:53 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+window.PARTES_CONFIG = {
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxpbbbaXzwRvtYMAoRuSgIxP9Vp-kK3bEh3TvREv0NCLveTcJgjTJjjrSEVcRgKhCHx/exec",
+  CONFIG_VERSION: "2026-05-27-google-sheets-nuevo",
+};
