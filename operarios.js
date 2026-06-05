02e99d38c315bff91df4cbc8d5d818f7636f34ee
igyomee <ig.yomee@gmail.com>
2026-06-05 08:17:21 +0200

    Add files via upload

---
+++ operarios.js
@@ -0,0 +1,322 @@
+window.PARTES_OPERARIOS = [
+    {
+        "codigo":  "21",
+        "nombre":  "IVAN CASTILLA RODRIGUEZ",
+        "dni":  "48312014P"
+    },
+    {
+        "codigo":  "65",
+        "nombre":  "MARIANO OLIVER GONZALEZ",
+        "dni":  "20833790E"
+    },
+    {
+        "codigo":  "87",
+        "nombre":  "ANTONIO MANUEL PROSPER",
+        "dni":  "44501559D"
+    },
+    {
+        "codigo":  "91",
+        "nombre":  "VICTOR MANUEL MARIN BAEZA",
+        "dni":  "70644102Q"
+    },
+    {
+        "codigo":  "94",
+        "nombre":  "DAVID VAÑO GARCIA",
+        "dni":  "52644564W"
+    },
+    {
+        "codigo":  "101",
+        "nombre":  "ANIBAL CARCEL AROCAS",
+        "dni":  "53055114W"
+    },
+    {
+        "codigo":  "120",
+        "nombre":  "JUAN JOSE ALBIÑANA LLORIA",
+        "dni":  "25380019W"
+    },
+    {
+        "codigo":  "142",
+        "nombre":  "FRANCISCO SAEZ ABAD",
+        "dni":  "73569415M"
+    },
+    {
+        "codigo":  "152",
+        "nombre":  "JOSE ANDRES CATALA",
+        "dni":  "33468852B"
+    },
+    {
+        "codigo":  "165",
+        "nombre":  "ARGEMIRO BOTERO VALENCIA",
+        "dni":  "54423296Y"
+    },
+    {
+        "codigo":  "173",
+        "nombre":  "JULIAN SALCEDO ANDRADE",
+        "dni":  "48303743V"
+    },
+    {
+        "codigo":  "188",
+        "nombre":  "MIGUEL MARTINEZ FERRANDO",
+        "dni":  "52659744W"
+    },
+    {
+        "codigo":  "189",
+        "nombre":  "ALBERTO DE LA CALLE GARCIA",
+        "dni":  "51406114X"
+    },
+    {
+        "codigo":  "241",
+        "nombre":  "SERGIO BELTRAN GONZALEZ",
+        "dni":  "52685624F"
+    },
+    {
+        "codigo":  "270",
+        "nombre":  "JAVIER GARCIA LOPEZ",
+        "dni":  "22642119E"
+    },
+    {
+        "codigo":  "301",
+        "nombre":  "JAIRO MENA ILLESCAS",
+        "dni":  "20989572W"
+    },
+    {
+        "codigo":  "302",
+        "nombre":  "ALVARO PEREZ ELEUTERIO",
+        "dni":  "44531907C"
+    },
+    {
+        "codigo":  "322",
+        "nombre":  "MAURICIO GUSTAVO PAEZ",
+        "dni":  "Y5501314G"
+    },
+    {
+        "codigo":  "326",
+        "nombre":  "JOSE MIGUEL MOLINA SOTO",
+        "dni":  "53358248L"
+    },
+    {
+        "codigo":  "333",
+        "nombre":  "IGNACIO JOSE MARTINEZ DE LEJARZA CAÑELLAS",
+        "dni":  "25384342R"
+    },
+    {
+        "codigo":  "337",
+        "nombre":  "RODRIGO JODAR MOTA",
+        "dni":  "53882920S"
+    },
+    {
+        "codigo":  "349",
+        "nombre":  "JOAQUIN JESUS BRAOJOS JIMENEZ",
+        "dni":  "49561740Y"
+    },
+    {
+        "codigo":  "353",
+        "nombre":  "MOHAMED SAHRAOUI",
+        "dni":  "Y0502582T"
+    },
+    {
+        "codigo":  "354",
+        "nombre":  "JUAN FRANCISCO LOPEZ IGEA",
+        "dni":  "24316316A"
+    },
+    {
+        "codigo":  "356",
+        "nombre":  "ISRAEL RUBIO BARCIA",
+        "dni":  "53363918P"
+    },
+    {
+        "codigo":  "362",
+        "nombre":  "CRISTIAN VOINESCU",
+        "dni":  "Y0423830T"
+    },
+    {
+        "codigo":  "366",
+        "nombre":  "ANDREI VOINESCU",
+        "dni":  "Y2815080D"
+    },
+    {
+        "codigo":  "376",
+        "nombre":  "BRIAN NICOLAS IOANNU",
+        "dni":  "X5079270L"
+    },
+    {
+        "codigo":  "379",
+        "nombre":  "NICUSOR ANDREI",
+        "dni":  "X3109597C"
+    },
+    {
+        "codigo":  "380",
+        "nombre":  "ALBERTO FERNANDEZ VICENTE",
+        "dni":  "48715544A"
+    },
+    {
+        "codigo":  "381",
+        "nombre":  "JOSE MIGUEL VALVERDE GIL",
+        "dni":  "23321165Q"
+    },
+    {
+        "codigo":  "382",
+        "nombre":  "RITCHI LEROY ICHUSTE ANGELETTI",
+        "dni":  "Y4947071S"
+    },
+    {
+        "codigo":  "383",
+        "nombre":  "EDUARD ANDRES RODRIGUEZ GERVAZZI",
+        "dni":  "Z0689155B"
+    },
+    {
+        "codigo":  "384",
+        "nombre":  "JOSE GREGORIO CRESPO MENDEZ",
+        "dni":  "Z0579972D"
+    },
+    {
+        "codigo":  "392",
+        "nombre":  "JOSE GONZALEZ BATALLER",
+        "dni":  "20812922S"
+    },
+    {
+        "codigo":  "395",
+        "nombre":  "CARLOS CABANES CARMONA",
+        "dni":  "20445713R"
+    },
+    {
+        "codigo":  "397",
+        "nombre":  "ANDRES MERLO BLANCA",
+        "dni":  "52675210N"
+    },
+    {
+        "codigo":  "401",
+        "nombre":  "JULIO FORTEA CASAÑ",
+        "dni":  "24368350B"
+    },
+    {
+        "codigo":  "405",
+        "nombre":  "MOHAMED MEZIANI",
+        "dni":  "X5140799T"
+    },
+    {
+        "codigo":  "410",
+        "nombre":  "MARTIN ADRIAN RODRIGUEZ PAZ",
+        "dni":  "Z1608852F"
+    },
+    {
+        "codigo":  "414",
+        "nombre":  "ADRIAN GALDON SOLIVA",
+        "dni":  "35597748G"
+    },
+    {
+        "codigo":  "419",
+        "nombre":  "VLADIMIRO KLEMENCIC",
+        "dni":  "Y9396642Y"
+    },
+    {
+        "codigo":  "421",
+        "nombre":  "MATIAS ZANETTI FONTAINE",
+        "dni":  "Z0981478G"
+    },
+    {
+        "codigo":  "423",
+        "nombre":  "JOSE LUIS MATEU MOTA",
+        "dni":  "29021251N"
+    },
+    {
+        "codigo":  "438",
+        "nombre":  "JESUS DEL PRADO ZAMORANO",
+        "dni":  "03906384H"
+    },
+    {
+        "codigo":  "439",
+        "nombre":  "JOSE FRANCISCO CALZADA SANCHEZ",
+        "dni":  "73099023D"
+    },
+    {
+        "codigo":  "441",
+        "nombre":  "JULIO ANTONIO COSTA MARTINEZ",
+        "dni":  "48439441S"
+    },
+    {
+        "codigo":  "442",
+        "nombre":  "ALEJANDRO GAMBORINO CATALAN",
+        "dni":  "23874372G"
+    },
+    {
+        "codigo":  "443",
+        "nombre":  "DANIEL GARZON GARRIDO",
+        "dni":  "73594008B"
+    },
+    {
+        "codigo":  "447",
+        "nombre":  "DAVID JAEN PASTOR",
+        "dni":  "48621001J"
+    },
+    {
+        "codigo":  "449",
+        "nombre":  "DIEGO PATRICIO SOSA",
+        "dni":  "Y1907965Z"
+    },
+    {
+        "codigo":  "450",
+        "nombre":  "DANIEL LUCAS PRIETO",
+        "dni":  "21512585H"
+    },
+    {
+        "codigo":  "451",
+        "nombre":  "JORGE GIMENEZ FARIÑAS",
+        "dni":  "24379840R"
+    },
+    {
+        "codigo":  "453",
+        "nombre":  "MANUEL GUTIERREZ VELASCO",
+        "dni":  "52748769V"
+    },
+    {
+        "codigo":  "454",
+        "nombre":  "JAVIER MULLOR GARCIA",
+        "dni":  "44867325Y"
+    },
+    {
+        "codigo":  "456",
+        "nombre":  "JAVIER FERNANDEZ BERNAD",
+        "dni":  "49181537Q"
+    },
+    {
+        "codigo":  "486",
+        "nombre":  "JESUS MAROTO ROMERO",
+        "dni":  "47045453B"
+    },
+    {
+        "codigo":  "487",
+        "nombre":  "JONATHAN MARTÍNEZ",
+        "dni":  "Z3254336W"
+    },
+    {
+        "codigo":  "488",
+        "nombre":  "JOSE LUIS NIEVA FENOLL",
+        "dni":  "53356443P"
+    },
+    {
+        "codigo":  "509",
+        "nombre":  "FRANCISCO GUTIERREZ ROSA",
+        "dni":  "51676077E"
+    },
+    {
+        "codigo":  "511",
+        "nombre":  "MIGUEL ANDRES MUJICA JIMENEZ",
+        "dni":  "Z3291793S"
+    },
+    {
+        "codigo":  "518",
+        "nombre":  "MAURICIO OCHOA JIMENEZ",
+        "dni":  "23317381G"
+    },
+    {
+        "codigo":  "520",
+        "nombre":  "DAVID PASCUALMARTINEZ",
+        "dni":  "21703080G"
+    },
+    {
+        "codigo":  "523",
+        "nombre":  "WILLIAM FERNANDO GUERRERO ROMAN",
+        "dni":  "Y9850621B"
+    }
+];
